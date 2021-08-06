fc7b17b64277babd2c62f318234f38e5ee07a4df
Kingsley Atuba <atubak40@gmail.com>
2021-08-06 12:43:32 +0100

    Quiz body design in progress

--- src/App.js
+++ src/App.js
@@ -1,7 +1,36 @@
 import React from 'react'
+import './app.css'
 
 function App() {
-  return <div className='App'>Quiz</div>
+  return (
+    <div className='app'>
+      <div className='main'>main</div>
+      <div className='pyramid'>
+        <div className='moneyList'>
+          <div className='moneyListItem active'>
+            <span className='moneyListItemNumber'>4</span>
+            <span className='moneyListItemAmount'>$ 400</span>
+          </div>
+          <div className='moneyListItem'>
+            <span className='moneyListItemNumber'>4</span>
+            <span className='moneyListItemAmount'>$ 400</span>
+          </div>
+          <div className='moneyListItem'>
+            <span className='moneyListItemNumber'>4</span>
+            <span className='moneyListItemAmount'>$ 400</span>
+          </div>
+          <div className='moneyListItem'>
+            <span className='moneyListItemNumber'>4</span>
+            <span className='moneyListItemAmount'>$ 400</span>
+          </div>
+          <div className='moneyListItem'>
+            <span className='moneyListItemNumber'>4</span>
+            <span className='moneyListItemAmount'>$ 400</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default App
